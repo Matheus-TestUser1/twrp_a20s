6cdc5be1dbcf3b7cc5a3c994097a2e751014f70f
Matheus Alves <99508668+Matheus-TestUser1@users.noreply.github.com>
2025-02-10 17:49:12 -0300

    Update mkbootimg.mk

--- mkboot/mkbootimg.mk
+++ mkboot/mkbootimg.mk
@@ -30,5 +30,5 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(recovery_ramdisk) $(recovery_k
 	@echo "----- Lying about SEAndroid state to Samsung bootloader ------"
 	$(hide) echo -n "SEANDROIDENFORCE" >> $@
 	@echo "Made recovery image: $@"
-	$(hide) tar --format=ustar -C $(PRODUCT_OUT) -c recovery.img > $(FLASH_IMAGE_TARGET)
+	$(hide) tar -C $(PRODUCT_OUT) -cf $(FLASH_IMAGE_TARGET) recovery.img
 	@echo "Made flashable $(FLASH_IMAGE_TARGET): $@"
